f5ca65fd18d564a2f692457092c14817d0c4a46e
xxDwayneJohnsonxx <100623596+xxDwayneJohnsonxx@users.noreply.github.com>
2022-09-07 09:20:13 -0700

    Add files via upload

---
+++ myvidagame/myvidagame/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace myvidagame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player1
+            // 
+            this.player1.Image = ((System.Drawing.Image)(resources.GetObject("player1.Image")));
+            this.player1.Location = new System.Drawing.Point(79, 180);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(189, 192);
+            this.player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            // 
+            // player2
+            // 
+            this.player2.Image = ((System.Drawing.Image)(resources.GetObject("player2.Image")));
+            this.player2.Location = new System.Drawing.Point(501, 180);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(189, 192);
+            this.player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player2.TabIndex = 3;
+            this.player2.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(28, 13);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(480, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // ground
+            // 
+            this.ground.Image = ((System.Drawing.Image)(resources.GetObject("ground.Image")));
+            this.ground.Location = new System.Drawing.Point(291, 366);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(64, 50);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 5;
+            this.ground.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(408, 204);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox player2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ myvidagame/myvidagame/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace myvidagame
+{
+   
+    public partial class Form1 : Form
+    {
+        player p1;
+        player p2;
+        ArrayList objects = new ArrayList();
+        public Form1()
+        {
+           
+            InitializeComponent();
+            p1 = new player(player1, this);
+            p2 = new player(player2, this);
+            
+            objects.Add(p1);
+            objects.Add(p2);
+        }
+        public bool isClear(PictureBox P, int X, int Y)
+        {
+            foreach (var item in Controls)
+            {
+                if (typeof(PictureBox) == item.GetType() && P != item)
+                {
+                    PictureBox pictureBox = (PictureBox)item;
+                    Rectangle newRect = new Rectangle(P.Location.X + X, P.Location.Y + Y, P.Width, P.Height);
+                    if (pictureBox.Bounds.IntersectsWith(newRect))
+                        return false;
+                }
+            }
+            return true;
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int m_speed = 5;
+           
+            {
+                m_speed = 30;
+               
+            }
+            if (e.KeyCode==Keys.A)
+            {
+                p1.moveleft();
+            }
+            if ( e.KeyCode == Keys.D)
+            {
+                p1.moveright();
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                p1.moveup();
+                
+            }
+            if ( e.KeyCode == Keys.S)
+            {
+                p1.movedown();
+               
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                p2.moveleft();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                p2.moveright();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                p2.moveup();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                p2.movedown();
+                isClear(player2, 5, 5);
+            }
+
+
+        }
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           if (progressBar1.Value < progressBar1.Maximum)
+                progressBar1.Value++;
+            foreach (player item in objects)
+            { 
+                if (!item.m_picture.Bounds.IntersectsWith(ground.Bounds))
+               item.tick();
+            
+            }
+            
+        }
+       
+
+    }
+}
